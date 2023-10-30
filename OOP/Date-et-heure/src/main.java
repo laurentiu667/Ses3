174d342934c192a608892914e5e3fe248bdf8d83
laurentiu667 <dilionlaur144@gmail.com>
2023-10-30 12:45:19 -0400

    sihdfesj

--- OOP/Date-et-heure/src/main.java
+++ OOP/Date-et-heure/src/main.java
@@ -1,16 +1,29 @@
 
+import javax.swing.*;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 public class main {
 
     public static void main(String[] args) {
-        LocalDateTime maintenant = LocalDateTime.now();
-        LocalDateTime dans3Jours = maintenant.plusDays(3);
-        Duration intervalle = Duration.between(maintenant, dans3Jours);
-        System.out.println("Temps écoulé en jours: " + intervalle.toDays());
-        System.out.println("Temps écoulé en heures: " + intervalle.toHours());
-        System.out.println("Temps écoulé en minutes: " + intervalle.toMinutes());
+        String modele = "yyyy-MM-dd HH-mm-ss";
+        LocalDateTime aujourdhui = LocalDateTime.now();
+        DateTimeFormatter date = DateTimeFormatter.ofPattern(modele);
+
+        System.out.println(date.format(aujourdhui));
+
+        LocalDate quebec =  LocalDate.of(1608, 7,3);
+        System.out.println("le quebec a ete fonder en : " + quebec.getDayOfMonth() + quebec.getMonth() + quebec.getYear());
+
+
+        System.out.println(aujourdhui.plusMonths(1));
+
+        LocalDateTime aujourdhuiSemaineHeure = LocalDateTime.now().plusWeeks(1).minusHours(1);
+        System.out.println(aujourdhuiSemaineHeure);
+
+        
+
 
     }
 }
