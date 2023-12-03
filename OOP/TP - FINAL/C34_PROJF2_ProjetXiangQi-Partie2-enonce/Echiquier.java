35ad24e6008715221729a8f2bcc21362855dc766
laurentiu667 <dilionlaur144@gmail.com>
2023-12-03 15:25:19 -0500

    looool

--- OOP/TP - FINAL/C34_PROJF2_ProjetXiangQi-Partie2-enonce/Echiquier.java
+++ OOP/TP - FINAL/C34_PROJF2_ProjetXiangQi-Partie2-enonce/Echiquier.java
@@ -28,7 +28,7 @@ public class Echiquier extends Intersection implements MethodesEchiquier {
     public void debuter() {
         // Placement des pièces noires
         // Roi
-        jeu[0][4].setPiece(new Mandarin("r", "noir"));
+        jeu[0][4].setPiece(new Roi("r", "noir"));
         // Char (Tour)
         jeu[0][0].setPiece(new Char("t1", "noir"));
         jeu[0][8].setPiece(new Char("t2", "noir"));
@@ -42,18 +42,18 @@ public class Echiquier extends Intersection implements MethodesEchiquier {
         jeu[0][3].setPiece(new Mandarin("m1", "noir"));
         jeu[0][5].setPiece(new Mandarin("m2", "noir"));
         // Bombarde (Canon)
-        jeu[2][1].setPiece(new Mandarin("b1", "noir"));
-        jeu[2][7].setPiece(new Mandarin("b2", "noir"));
+        jeu[2][1].setPiece(new Bombarde("b1", "noir"));
+        jeu[2][7].setPiece(new Bombarde("b2", "noir"));
         // Pion
-        jeu[3][0].setPiece(new Mandarin("p1", "noir"));
-        jeu[3][2].setPiece(new Mandarin("p2", "noir"));
-        jeu[3][4].setPiece(new Mandarin("p3", "noir"));
-        jeu[3][6].setPiece(new Mandarin("p4", "noir"));
-        jeu[3][8].setPiece(new Mandarin("p5", "noir"));
+        jeu[3][0].setPiece(new Pion("p1", "noir"));
+        jeu[3][2].setPiece(new Pion("p2", "noir"));
+        jeu[3][4].setPiece(new Pion("p3", "noir"));
+        jeu[3][6].setPiece(new Pion("p4", "noir"));
+        jeu[3][8].setPiece(new Pion("p5", "noir"));
         
         // Placement des pièces rouges
         // Roi
-        jeu[9][4].setPiece(new Mandarin("r", "rouge"));
+        jeu[9][4].setPiece(new Roi("r", "rouge"));
         // Char (Tour)
         jeu[9][0].setPiece(new Char("t1", "rouge"));
         jeu[9][8].setPiece(new Char("t2", "rouge"));
@@ -67,19 +67,25 @@ public class Echiquier extends Intersection implements MethodesEchiquier {
         jeu[9][3].setPiece(new Mandarin("m1", "rouge"));
         jeu[9][5].setPiece(new Mandarin("m2", "rouge"));
         // Bombarde (Canon)
-        jeu[7][1].setPiece(new Mandarin("b1", "rouge"));
-        jeu[7][7].setPiece(new Mandarin("b2", "rouge"));
+        jeu[7][1].setPiece(new Bombarde("b1", "rouge"));
+        jeu[7][7].setPiece(new Bombarde("b2", "rouge"));
         // Pion
-        jeu[6][0].setPiece(new Mandarin("p1", "rouge"));
-        jeu[6][2].setPiece(new Mandarin("p2", "rouge"));
-        jeu[6][4].setPiece(new Mandarin("p3", "rouge"));
-        jeu[6][6].setPiece(new Mandarin("p4", "rouge"));
-        jeu[6][8].setPiece(new Mandarin("p5", "rouge"));
+        jeu[6][0].setPiece(new Pion("p1", "rouge"));
+        jeu[6][2].setPiece(new Pion("p2", "rouge"));
+        jeu[6][4].setPiece(new Pion("p3", "rouge"));
+        jeu[6][6].setPiece(new Pion("p4", "rouge"));
+        jeu[6][8].setPiece(new Pion("p5", "rouge"));
 
     }
 
     @Override
     public boolean cheminPossible(Position depart, Position arrivee) {
+        Intersection interesectiondDepart = getIntersection(depart.getLigne(), depart.getColonne());
+        Intersection interesectiondArriver = getIntersection(arrivee.getLigne(), arrivee.getColonne());
+
+        if(interesectiondDepart.getPiece().getCouleur() == interesectiondArriver.getPiece().getCouleur()){
+            return false;
+        }
 
         return false;
     }
@@ -87,12 +93,23 @@ public class Echiquier extends Intersection implements MethodesEchiquier {
 
     @Override
     public boolean roisNePouvantPasEtreFaceAFace(Position posPremierRoi, Position posDeuxiemeRoi) {
-
+        // Roi est dans la meme colonne
         if (posPremierRoi.getColonne() == posDeuxiemeRoi.getColonne()){
-            
+
+            // Detection d une piece entre les deux rois
+            // Commencer la detection une case apres la position du roi
+            for (int i = posPremierRoi.getLigne() + 1; i <= posDeuxiemeRoi.getLigne(); i++){
+                if (jeu[i][posPremierRoi.getColonne()].getPiece() != null){
+                    return false;
+                }
+            }
+            // Pas de piece detecter
+            return true;
         }
+        else {
+            // Roi ne sont pas face a face
+            return true;
 
-
-        return false;
+        }
     }
 }
